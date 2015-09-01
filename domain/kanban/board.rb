275205218960f0468a6be148754dc4518a34a7be
haazime <h4zime@gmail.com>
2015-09-01 22:37:43 +0900

    fix

--- domain/kanban/board.rb
+++ domain/kanban/board.rb
@@ -1,10 +1,8 @@
 module Kanban
   class WipLimitReached < StandardError; end
 
-  class Board# < ActiveRecord::Base
-    #include Arize::Board
-
-    attr_reader :project_id
+  class Board < ActiveRecord::Base
+    include Arize::Board
 
     def add_card(feature_id, rule)
       card = Card.write(feature_id)
@@ -26,8 +24,7 @@ module Kanban
     # for AR::Base
 
     def prepare(project_id)
-      #self.project_id_str = project_id.to_s
-      @project_id = project_id
+      self.project_id_str = project_id.to_s
       @stage = Kanban::Stage.new
     end
 
@@ -35,19 +32,8 @@ module Kanban
       @stage
     end
 
-    def stages=(stages)
-      #stages.to_a.each do |stage|
-      #  stage_records.build(
-      #    phase_description: stage.phase.to_s,
-      #    wip_limit_count: stage.wip_limit.to_i,
-      #  )
-      #end
-      @stages = stages
-    end
-
     def project_id
-      @project_id
-      #Project::ProjectId.new(self.project_id_str)
+      Project::ProjectId.new(self.project_id_str)
     end
   end
 end
--- spec/domain/kanban/board/save_spec.rb
+++ spec/domain/kanban/board/save_spec.rb
@@ -2,42 +2,16 @@ require 'rails_helper'
 
 module Kanban
   describe Board do
-    describe '.save!' do
+    describe '#save!' do
       subject do
+        described_class
+          .new
+          .tap {|board| board.prepare(project_id) }
+          .save!
         described_class.find_by(project_id_str: project_id.to_s)
       end
 
-      before do
-        workflow
-          .build_board_with(builder)
-          .save!
-      end
-
       let(:project_id) { Project::ProjectId.new('prj_123') }
-      let(:builder) { Kanban::BoardBuilder.new(project_id) }
-
-      let(:aworkflow) do
-        Project::Workflow.new([
-          Project::PhaseSpec.new(
-            Project::Phase.new('Todo'),
-            Project::Transition::None.new,
-            Project::WipLimit.new(10)
-          ),
-          Project::PhaseSpec.new(
-            Project::Phase.new('Dev'),
-            Project::Transition.new([
-              Project::State.new('Doing'),
-              Project::State.new('Done')
-            ]),
-            Project::WipLimit.new(2)
-          ),
-          Project::PhaseSpec.new(
-            Project::Phase.new('QA'),
-            Project::Transition::None.new,
-            Project::WipLimit.new(1)
-          )
-        ])
-      end
 
       it { is_expected.to_not be_nil }
     end
