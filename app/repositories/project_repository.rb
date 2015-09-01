c78cdf19678ded404d60a475b775373b7880c68c
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-01 12:27:57 +0900

    eager load

--- app/repositories/project_repository.rb
+++ app/repositories/project_repository.rb
@@ -1,7 +1,9 @@
 class ProjectRepository
 
   def find(project_id)
-    ::Project::Project.find_by(project_id_str: project_id.to_s)
+    ::Project::Project
+      .includes(:phase_spec_records, :state_records)
+      .find_by(project_id_str: project_id.to_s)
   end
 
   def store(project)
