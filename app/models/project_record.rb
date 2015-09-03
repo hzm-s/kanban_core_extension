e7d559c6eaafd6a3d78e04ade2b90b3baeaaa687
haazime <h4zime@gmail.com>
2015-09-04 01:51:52 +0900

    avoid N+1

--- app/models/project_record.rb
+++ app/models/project_record.rb
@@ -1,11 +1,11 @@
 class ProjectRecord < ActiveRecord::Base
-  has_many :phase_spec_records
-  has_many :state_records
+  has_many :phase_spec_records, -> { order(:order) }
+  has_many :state_records, -> { order(:order) }
 
   class << self
 
     def with_workflow(project_id_str)
-      includes(:phase_spec_records, :state_records)
+      eager_load(:phase_spec_records, :state_records)
         .find_by(project_id_str: project_id_str)
     end
   end
--- app/view_models/board_header_view.rb
+++ app/view_models/board_header_view.rb
@@ -12,8 +12,16 @@ BoardHeaderView = Struct.new(:project_name, :phases, :phase_states) do
 
     def initialize(project_with_workflow)
       @project_with_workflow = project_with_workflow
-      @phase_specs = project_with_workflow.phase_spec_records.order(:order)
-      @states = project_with_workflow.state_records.order(:order)
+      @phase_specs = project_with_workflow.phase_spec_records.to_a
+
+      states = project_with_workflow.state_records.to_a
+      @states_map = @phase_specs.each_with_object({}) do |phase_spec, h|
+        if (phase_states = states.select {|s| s.phase_name == phase_spec.phase_name }).any?
+          h[phase_spec.phase_name] = phase_states
+        else
+          h[phase_spec.phase_name] = []
+        end
+      end
     end
 
     def generate
@@ -29,18 +37,18 @@ BoardHeaderView = Struct.new(:project_name, :phases, :phase_states) do
         Phase.new(
           phase_spec.phase_name,
           phase_spec.wip_limit_count,
-          @states.where(phase_name: phase_spec.phase_name).size
+          @states_map[phase_spec.phase_name].size
         )
       end
     end
 
     def phase_states
-      @phase_specs.map(&:phase_name).flat_map do |phase_name|
-        if (states = @states.where(phase_name: phase_name)).any?
-          states.pluck(:state_name)
-        else
-          ''
-        end
+      @states_map.values.inject([]) do |a, states|
+        a += if states.any?
+               states.flat_map(&:state_name)
+             else
+               ['']
+             end
       end
     end
   end
