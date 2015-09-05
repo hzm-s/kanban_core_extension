7a3f75b45c261a9d75067229af436c28c33afa2c
haazime <h4zime@gmail.com>
2015-09-05 18:22:02 +0900

    not use

--- domain/kanban/rule.rb
+++ domain/kanban/rule.rb
@@ -16,18 +16,6 @@ module Kanban
       !@workflow.reach_wip_limit?(phase, card_size)
     end
 
-    def valid_positions_for_pull?(from_position, to_position)
-      from_situation = Project::Situation.new(*from_position.to_a)
-      to_situation = Project::Situation.new(*to_position.to_a)
-      @workflow.correct?(from_situation, to_situation)
-    end
-
-    def valid_positions_for_push?(from_position, to_position)
-      from_situation = Project::Situation.new(*from_position.to_a)
-      to_situation = Project::Situation.new(*to_position.to_a)
-      @workflow.correct_transition?(from_situation, to_situation)
-    end
-
     def initial_position
       situation = @workflow.first_situation
       Position.new(situation.phase, situation.state)
