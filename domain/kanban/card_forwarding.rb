e490cafaea138a58ee9af07461406deccfac9f6f
haazime <h4zime@gmail.com>
2015-09-07 14:10:10 +0900

    refactor

--- domain/kanban/card_forwarding.rb
+++ domain/kanban/card_forwarding.rb
@@ -6,12 +6,8 @@ module Kanban
       next_stage = rule.next_stage(current_stage)
 
       return CardRemoving.new(current_stage, next_stage) if next_stage.complete?
-
-      if current_stage.same_phase?(next_stage)
-        CardPushing.new(current_stage, next_stage)
-      else
-        CardPulling.new(current_stage, next_stage, rule)
-      end
+      return CardPushing.new(current_stage, next_stage) if current_stage.same_phase?(next_stage)
+      CardPulling.new(current_stage, next_stage, rule)
     end
   end
 end
