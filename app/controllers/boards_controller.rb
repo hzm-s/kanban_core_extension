df38c15ec6fff2faa57b716861c7d101888513af
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-08 12:04:37 +0900

    fix statistics

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,11 +3,7 @@ class BoardsController < ApplicationController
 
   def show
     @backlogged_count = BackloggedFeatureRecord.count(params[:project_id_str])
-    @shipped_count = FeatureRecord
-                       .includes(:shipped_feature_record)
-                       .where(project_id_str: params[:project_id_str])
-                       .where.not(shipped_feature_records: { id: nil })
-                       .count
+    @shipped_count = ShippedFeatureRecord.count(params[:project_id_str])
     @board = View::Board.build(params[:project_id_str])
   end
 end
--- app/models/card_record.rb
+++ app/models/card_record.rb
@@ -1,18 +1,27 @@
 class CardRecord < ActiveRecord::Base
+  class << self
 
-  def self.with_feature(project_id_str)
-    sql = <<-EOSQL
-      SELECT
-        card.*, feature.*
-      FROM
-        board_records AS board
-          JOIN card_records AS card
-            ON board.id = card.board_record_id
-          JOIN feature_records AS feature
-            ON card.feature_id_str = feature.feature_id_str
-      WHERE
-        board.project_id_str = ?
-    EOSQL
-    connection.select_all(sanitize_sql_array([sql, project_id_str])).to_hash
+    def count(project_id_str)
+      BoardRecord
+        .includes(:card_records)
+        .where(project_id_str: project_id_str)
+        .count
+    end
+
+    def with_feature(project_id_str)
+      sql = <<-EOSQL
+        SELECT
+          card.*, feature.*
+        FROM
+          board_records AS board
+            JOIN card_records AS card
+              ON board.id = card.board_record_id
+            JOIN feature_records AS feature
+              ON card.feature_id_str = feature.feature_id_str
+        WHERE
+          board.project_id_str = ?
+      EOSQL
+      connection.select_all(sanitize_sql_array([sql, project_id_str])).to_hash
+    end
   end
 end
