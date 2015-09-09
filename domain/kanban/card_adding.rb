1e158dc0699660ed11c859f60a7255fca5655c23
haazime <h4zime@gmail.com>
2015-09-09 22:12:53 +0900

    refactor

--- domain/kanban/card_adding.rb
+++ domain/kanban/card_adding.rb
@@ -4,25 +4,21 @@ module Kanban
     def initialize(feature_id, rule)
       @feature_id = feature_id
       @rule = rule
+      @first_progress = @rule.first_progress
+      @first_phase = @first_progress.phase
     end
 
     def handle_board(board)
-      first_phase_cards = board.count_card(first_progress.phase)
-      raise WipLimitReached unless @rule.can_put_card?(first_progress.phase, first_phase_cards)
+      first_phase_cards = board.count_card(@first_phase)
+      raise WipLimitReached unless @rule.can_put_card?(@first_phase, first_phase_cards)
 
       card = Card.write(@feature_id)
-      board.put_card(card, first_progress)
+      board.put_card(card, @first_progress)
 
       EventPublisher.publish(
         :card_added,
         CardAdded.new(board.project_id, card)
       )
     end
-
-    private
-
-      def first_progress
-        @first_progress ||= @rule.first_progress
-      end
   end
 end
