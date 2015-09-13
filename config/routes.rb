2921677674f22a647b879c6b89528f9265a39af4
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-13 15:52:29 +0900

    for change wip limit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :card_addings, only: [:create]
   resources :card_forwardings, only: [:create]
+  resources :wip_limit_changings, only: [:create]
 
   get 'backlogs/:project_id_str', to: 'backlogs#show', as: :backlog
   get 'boards/:project_id_str', to: 'boards#show', as: :board
