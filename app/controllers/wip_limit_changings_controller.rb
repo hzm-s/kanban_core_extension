ff8b04f710ad0fab7154a10005aea530f63e9bd3
haazime <h4zime@gmail.com>
2015-09-14 22:01:59 +0900

    dynamic render form to modal

--- app/controllers/wip_limit_changings_controller.rb
+++ app/controllers/wip_limit_changings_controller.rb
@@ -1,5 +1,13 @@
 class WipLimitChangingsController < ApplicationController
 
+  def new
+    @command = ChangeWipLimitCommand.new(
+      project_id_str: params[:project_id_str],
+      phase_name: params[:phase_name]
+    )
+    render 'modal_forms/new_wip_limit_changing'
+  end
+
   def create
     command = ChangeWipLimitCommand.new(params[:change_wip_limit_command])
     if command.execute(project_service)
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   resources :projects, only: [:index]
   resources :project_launchings, only: [:new, :create]
   resources :features, only: [:new, :create]
-  resources :workflows, only: [:new, :create]
 
   resources :card_addings, only: [:create]
   resources :card_forwardings, only: [:create]
-  resources :wip_limit_changings, only: [:create]
+
+  resources :wip_limit_changings, only: [:new, :create]
 
   get 'backlogs/:project_id_str', to: 'backlogs#show', as: :backlog
   get 'boards/:project_id_str', to: 'boards#show', as: :board
