7d1a1923dd24cb4721e20c078b92dc4b16c0cbf9
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-15 10:41:28 +0900

    validation error helper for modal window

--- app/controllers/phase_spec_addings_controller.rb
+++ app/controllers/phase_spec_addings_controller.rb
@@ -10,11 +10,12 @@ class PhaseSpecAddingsController < ApplicationController
   end
 
   def create
-    command = AddPhaseSpecCommand.new(params[:add_phase_spec_command])
-    if command.execute(workflow_service)
-      redirect_to board_url(project_id_str: command.project_id_str), notice: 'ワークフローにフェーズを追加しました。'
+    @command = AddPhaseSpecCommand.new(params[:add_phase_spec_command])
+    if @command.execute(workflow_service)
+      flash[:notice] = 'ワークフローにフェーズを追加しました。'
+      render 'redirect_from_modal', locals: { to: board_url(@command.project_id_str) }
     else
-      redirect_to board_url(project_id_str: command.project_id_str), alert: command.errors.full_messages.join('<br>')
+      render 'modal_window_form', locals: { path: 'boards/new_phase_spec_adding' }
     end
   end
 end
