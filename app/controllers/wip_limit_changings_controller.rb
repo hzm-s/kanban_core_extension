3175c4f0291551c4c3778f8f678d7c1b4d0c89f8
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-15 10:31:20 +0900

    handle request from modal

--- app/controllers/wip_limit_changings_controller.rb
+++ app/controllers/wip_limit_changings_controller.rb
@@ -9,11 +9,12 @@ class WipLimitChangingsController < ApplicationController
   end
 
   def create
-    command = ChangeWipLimitCommand.new(params[:change_wip_limit_command])
-    if command.execute(project_service)
-      redirect_to board_url(command.project_id_str), notice: 'WIP Limit Changed'
+    @command = ChangeWipLimitCommand.new(params[:change_wip_limit_command])
+    if @command.execute(project_service)
+      flash[:notice] = 'WIP制限の値を変更しました。'
+      render 'redirect_from_modal', locals: { to: board_url(@command.project_id_str) }
     else
-      redirect_to board_url(command.project_id_str), alert: command.errors.full_messages.join('<br>')
+      render 'modal_window_form', locals: { path: 'boards/new_wip_limit_changing' }
     end
   end
 end
