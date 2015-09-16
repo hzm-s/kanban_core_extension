ae582b80f6b4b5fe6e57a1fc438d4cfe06341788
haazime <h4zime@gmail.com>
2015-09-16 21:29:42 +0900

    add spec for remove state from 3states

--- spec/services/workflow_service/remove_state_spec.rb
+++ spec/services/workflow_service/remove_state_spec.rb
@@ -9,7 +9,30 @@ describe 'add state' do
 
   let(:project_id) { Project('Name', 'Goal') }
 
+  let(:board_service) do
+    BoardService(board_repository: board_repository, development_tracker: FakeDevelopmentTracker.new)
+  end
+
   before do
     ProjectService().specify_workflow(project_id, workflow)
+  end
+
+  let(:phase) { Phase('Dev') }
+
+  context 'states = Doing|Review|Done' do
+    let(:workflow) do
+      Workflow([{ phase: phase, transition: ['Doing', 'Review', 'Done'] }])
+    end
+
+    context 'no card' do
+      it do
+        state = State.new('Review')
+        service.remove_state(project_id, phase, state)
+        new_workflow = project_repository.find(project_id).workflow
+        expect(new_workflow).to eq(
+          Workflow([{ phase: phase, transition: ['Doing', 'Done'] }])
+        )
+      end
+    end
   end
 end
