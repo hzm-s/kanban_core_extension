af64d3e24371b7e152d2cf5ecdd8aa26857988dc
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-16 16:21:43 +0900

    AddStateCommand

--- app/commands/add_state_command.rb
+++ app/commands/add_state_command.rb
@@ -1,10 +1,20 @@
 class AddStateCommand
   include ActiveModel::Model
 
-  attr_accessor :project_id_str, :phase_name, :state_name
+  attr_accessor :project_id_str, :phase_name, :state_name,
+                :direction, :base_state_name
 
   validates :project_id_str, presence: true
   validates :state_name, presence: true
+  validates :direction, presence: true
+  validates :base_state_name, presence: true
+
+  DIRECTIONS = { 'before' => '前', 'after' => '後' }.freeze
+
+  def describe
+    position_name = DIRECTIONS[direction]
+    "「#{phase_name}」フェーズの「#{base_state_name}」の#{position_name}に状態を追加"
+  end
 
   def project_id
     Project::ProjectId.new(project_id_str)
@@ -18,8 +28,12 @@ class AddStateCommand
     Project::State.new(state_name)
   end
 
+  def position_option
+    { direction.to_sym => Project::Phase.new(base_state_name) }
+  end
+
   def execute(service)
     return false unless valid?
-    service.add_state(project_id, phase, state)
+    service.add_state(project_id, phase, state, position_option)
   end
 end
--- spec/commands/add_state_command_spec.rb
+++ spec/commands/add_state_command_spec.rb
@@ -2,29 +2,64 @@ require 'rails_helper'
 
 describe AddStateCommand do
   describe '#execute' do
-    context 'state_name:Doing' do
+    context 'state_name = Review direction = before base_state_name: Done' do
       it do
         cmd = described_class.new(
           project_id_str: 'prj_789',
           phase_name: 'Dev',
-          state_name: 'Doing'
+          state_name: 'Review',
+          direction: 'before',
+          base_state_name: 'Done'
         )
         service = double(:workflow_service)
         expect(service).to receive(:add_state).with(
           ProjectId('prj_789'),
           Phase('Dev'),
-          State('Doing')
+          State('Review'),
+          { before: Phase('Done') }
         )
         cmd.execute(service)
       end
     end
 
-    context 'state_name:""' do
+    context 'state_name = Review direction = after base_state_name: Doing' do
+      it do
+        cmd = described_class.new(
+          project_id_str: 'prj_789',
+          phase_name: 'Dev',
+          state_name: 'Review',
+          direction: 'after',
+          base_state_name: 'Doing'
+        )
+        service = double(:workflow_service)
+        expect(service).to receive(:add_state).with(
+          ProjectId('prj_789'),
+          Phase('Dev'),
+          State('Review'),
+          { after: Phase('Doing') }
+        )
+        cmd.execute(service)
+      end
+    end
+
+    context 'state_name = ""' do
       it do
         cmd = described_class.new(
           project_id_str: 'prj_789',
           phase_name: 'Dev',
           state_name: ''
+        )
+        service = double(:workflow_service)
+        expect(cmd.execute(service)).to be_falsey
+      end
+    end
+
+    context 'state_name = Review' do
+      it do
+        cmd = described_class.new(
+          project_id_str: 'prj_789',
+          phase_name: 'Dev',
+          state_name: 'Review'
         )
         service = double(:workflow_service)
         expect(cmd.execute(service)).to be_falsey
@@ -43,6 +78,32 @@ describe AddStateCommand do
     it do
       cmd = described_class.new(phase_name: 'Dev')
       expect(cmd.phase).to eq(Phase('Dev'))
+    end
+  end
+
+  describe '#describe' do
+    context 'state_name = Review direction = before base_state_name: Done' do
+      it do
+        cmd = described_class.new(
+          phase_name: 'Dev',
+          direction: 'before',
+          base_state_name: 'Done'
+        )
+        expect(cmd.describe).to eq('「Dev」フェーズの「Done」の前に状態を追加')
+      end
+    end
+  end
+
+  describe '#describe' do
+    context 'state_name = Review direction = after base_state_name: Doing' do
+      it do
+        cmd = described_class.new(
+          phase_name: 'Dev',
+          direction: 'after',
+          base_state_name: 'Doing'
+        )
+        expect(cmd.describe).to eq('「Dev」フェーズの「Doing」の後に状態を追加')
+      end
     end
   end
 end
