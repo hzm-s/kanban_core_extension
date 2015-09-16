d5ddb1bf1dcdb3f29b8c182c2bd6995fd909f6fc
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-16 16:34:10 +0900

    state adding modal form

--- app/controllers/state_addings_controller.rb
+++ app/controllers/state_addings_controller.rb
@@ -1,5 +1,12 @@
 class StateAddingsController < ApplicationController
 
   def new
+    @command = AddStateCommand.new(
+      project_id_str: params[:project_id_str],
+      phase_name: params[:phase_name],
+      direction: params[:direction],
+      base_state_name: params[:base_state_name]
+    )
+    render 'modal_window_form', locals: { path: 'boards/new_state_adding' }
   end
 end
