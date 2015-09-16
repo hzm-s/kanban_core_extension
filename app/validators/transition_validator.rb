f971771f4ef63cca6c67a0daf117e931aebb42ee
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-09-16 14:15:44 +0900

    fix transition validation

---
+++ app/validators/transition_validator.rb
@@ -0,0 +1,14 @@
+class TransitionValidator < ActiveModel::EachValidator
+
+  def validate_each(model, _, value)
+    Project::Transition.from_array(value)
+  rescue ArgumentError
+    model.errors.add(:base, message)
+  end
+
+  private
+
+    def message
+      options[:message] || '状態は2つ以上必要です'
+    end
+end
