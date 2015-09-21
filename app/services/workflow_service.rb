063f83b52ededcbf116042d05c6873a3c513d535
haazime <h4zime@gmail.com>
2015-09-21 21:41:22 +0900

    use PhaseSpecFactory#change_wip_limit

--- app/services/workflow_service.rb
+++ app/services/workflow_service.rb
@@ -9,9 +9,12 @@ class WorkflowService
     project = @project_repository.find(project_id)
     board = @board_repository.find(project_id)
 
-    old = project.workflow
-    new = old.change_wip_limit(phase, new_wip_limit, board)
-    project.specify_workflow(new)
+    workflow_factory = Activity::WorkflowFactory.new(project.workflow)
+    phase_spec_factory = Activity::PhaseSpecFactory.new(project.workflow.spec(phase))
+
+    phase_spec_factory.change_wip_limit(new_wip_limit, board)
+    workflow_factory.replace_phase_spec(phase_spec_factory.build_phase_spec, phase)
+    project.specify_workflow(workflow_factory.build_workflow)
 
     @project_repository.store(project)
   end
