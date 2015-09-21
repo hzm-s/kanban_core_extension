23255f235eda31e2f27466b67f0c09f45bde4fde
haazime <h4zime@gmail.com>
2015-09-21 19:55:12 +0900

    remove state use PhaseSpecFactory

--- app/services/workflow_service.rb
+++ app/services/workflow_service.rb
@@ -93,9 +93,12 @@ class WorkflowService
     project = @project_repository.find(project_id)
     board = @board_repository.find(project_id)
 
-    old = project.workflow
-    new = old.remove_state(phase, state, board)
-    project.specify_workflow(new)
+    workflow_factory = Activity::WorkflowFactory.new(project.workflow)
+    phase_spec_factory = Activity::PhaseSpecFactory.new(project.workflow.spec(phase))
+
+    phase_spec_factory.remove_state(state, board)
+    workflow_factory.replace_phase_spec(phase_spec_factory.build_phase_spec, phase)
+    project.specify_workflow(workflow_factory.build_workflow)
 
     @project_repository.store(project)
   end
