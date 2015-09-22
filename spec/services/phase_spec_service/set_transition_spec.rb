59629ffdfda7ab50e33132c91004e96b40f04fb1
haazime <h4zime@gmail.com>
2015-09-23 00:15:59 +0900

    add spec for automatic card putting when transition setted

--- spec/services/phase_spec_service/set_transition_spec.rb
+++ spec/services/phase_spec_service/set_transition_spec.rb
@@ -5,9 +5,13 @@ describe 'set transition' do
     PhaseSpecService.new(project_repository, board_repository)
   end
   let(:project_repository) { ProjectRepository.new }
-  let(:board_repository) { FakeBoardRepository.new }
+  let(:board_repository) { BoardRepository.new }
 
   let(:project_id) { Project('Name', 'Goal') }
+
+  let(:board_service) do
+    BoardService(development_tracker: FakeDevelopmentTracker.new)
+  end
 
   before do
     ProjectService().specify_workflow(project_id, workflow)
@@ -23,19 +27,49 @@ describe 'set transition' do
       ])
     end
 
-    context 'set Doing|Done' do
-      it do
-        service.set_transition(
-          project_id,
-          Phase('Dev'),
-          [State('Doing'), State('Done')]
-        )
-        expect(new_workflow).to eq(
-          Workflow([
-            { phase: 'Dev', transition: ['Doing', 'Done'], wip_limit: 2 },
-            { phase: 'QA', transition: ['Doing', 'Done'] }
-          ])
-        )
+    context 'no card' do
+      context 'set Doing|Done' do
+        it do
+          service.set_transition(
+            project_id,
+            Phase('Dev'),
+            [State('Doing'), State('Done')]
+          )
+          expect(new_workflow).to eq(
+            Workflow([
+              { phase: 'Dev', transition: ['Doing', 'Done'], wip_limit: 2 },
+              { phase: 'QA', transition: ['Doing', 'Done'] }
+            ])
+          )
+        end
+      end
+    end
+
+    context 'phase on card' do
+      before do
+        board_service.add_card(project_id, feature_id)
+      end
+
+      let(:feature_id) { FeatureId('feat_123') }
+
+      context 'set Doing|Done' do
+        it do
+          service.set_transition(
+            project_id,
+            Phase('Dev'),
+            [State('Doing'), State('Done')]
+          )
+
+          board = board_repository.find(project_id)
+          expect(board.fetch_card(feature_id, Step('Dev', 'Doing'))).to_not be_nil
+
+          expect(new_workflow).to eq(
+            Workflow([
+              { phase: 'Dev', transition: ['Doing', 'Done'], wip_limit: 2 },
+              { phase: 'QA', transition: ['Doing', 'Done'] }
+            ])
+          )
+        end
       end
     end
   end
